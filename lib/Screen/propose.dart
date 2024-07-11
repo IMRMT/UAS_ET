6212255d1d0ca814de6ab267490fea1a32488b05
theodorusRH <s160421144@student.ubaya.ac.id>
2024-07-11 16:48:28 +0700

    propose
    id blm

--- lib/Screen/propose.dart
+++ lib/Screen/propose.dart
@@ -1,12 +1,28 @@
+import 'dart:convert';
+
+import 'package:et_uas/Screen/browse.dart';
 import 'package:flutter/material.dart';
 import 'package:et_uas/Class/animal.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
-class Propose extends StatelessWidget {
+class Propose extends StatefulWidget {
   final Animal animal;
 
   Propose({required this.animal});
 
-  String getGenderString(AnimalGender gender) {//ini ambil dari class untuk di convert
+  @override
+  _ProposeState createState() => _ProposeState();
+}
+
+class _ProposeState extends State<Propose> {
+  final _formKey = GlobalKey<FormState>();
+  String _keterangan = "";
+
+  //buat ambil id
+  String _activeUser="";
+
+  String getGenderString(AnimalGender gender) {
     switch (gender) {
       case AnimalGender.jantan:
         return 'Jantan';
@@ -17,52 +33,127 @@ class Propose extends StatelessWidget {
     }
   }
 
+  _getActiveUser() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _activeUser = prefs.getString("user_id") ?? "";
+    });
+  }
+
+  void submit() async {
+    final response = await http
+        .post(Uri.parse("https://ubaya.me/flutter/160421056/uas/submitPropose.php"), body: {
+      'keterangan': _keterangan,
+      'user_id':_getActiveUser(),
+    });
+    if (response.statusCode == 200) {
+      Map json = jsonDecode(response.body);
+      if (json['result'] == 'success') {
+        if (!mounted) return;
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text('Sukses Menambah Data')));
+        Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => Browse()),
+        );
+      }
+    } else {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('Error')));
+      throw Exception('Failed to read API');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Propose to Adopt ${animal.name}'),
+        title: Text('Propose to Adopt ${widget.animal.name}'),
       ),
-      body: SingleChildScrollView(
+      body: ListView(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              'Name: ${animal.name}',
-              style: TextStyle(fontSize: 24),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: Image.network(
-                animal.image_url,
-                fit: BoxFit.cover,
+        children: <Widget>[
+          Form(
+            key: _formKey,
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Name: ${widget.animal.name}',
+                    style: TextStyle(fontSize: 24),
+                    textAlign: TextAlign.center,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: SizedBox(
+                      width: 200,
+                      height: 200,
+                      child: Image.network(
+                        widget.animal.image_url,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    'Type: ${widget.animal.tipe}',
+                    style: TextStyle(fontSize: 18),
+                    textAlign: TextAlign.center,
+                  ),
+                  Text(
+                    'Gender: ${getGenderString(widget.animal.gender)}',
+                    style: TextStyle(fontSize: 18),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 16),
+                  Text(
+                    'Age: ${widget.animal.umur}',
+                    style: TextStyle(fontSize: 18),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 16),
+                  // INI KETERANGAN
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'Keterangan',
+                      ),
+                      onChanged: (value) {
+                        _keterangan = value;
+                      },
+                      keyboardType: TextInputType.multiline,
+                      minLines: 3,
+                      maxLines: 6,
+                      validator: (value) {
+                        if (value == null || value.length < 10) {
+                          return 'Harus lebih dari 10 karakter';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState != null &&
+                            !_formKey.currentState!.validate()) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text('Harap Isian diperbaiki')));
+                        } else {
+                          submit();
+                        }
+                      },
+                      child: Text('Submit'),
+                    ),
+                  ),
+                ],
               ),
             ),
-            Text(
-              'Type: ${animal.tipe}',
-              style: TextStyle(fontSize: 18),
-            ),
-            Text(
-              'Gender: ${getGenderString(animal.gender)}',//ini nampilin hasil convert
-              style: TextStyle(fontSize: 18),
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Age: ${animal.umur}',
-              style: TextStyle(fontSize: 18),
-            ),
-            SizedBox(height: 16),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  // Handle the proposal action
-                },
-                child: Text('Propose to Adopt'),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
